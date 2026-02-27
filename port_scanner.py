5ddda559bd9b6e1ced96063edeac54c9e691c8fd
Bhagyeshteli-cmd <bhagyeshteli0@gmail.com>
2026-02-27 02:25:17 -0500

    Upgraded to multi-threaded port scanner

--- port_scanner.py
+++ port_scanner.py
@@ -1,17 +1,39 @@
 import socket
+import threading
+from queue import Queue
 
-target = input("Enter target IP: ")
+target = input("Enter target IP or domain: ")
+start_port = int(input("Start port: "))
+end_port = int(input("End port: "))
 
-print("Scanning started...")
+queue = Queue()
 
-for port in range(1, 1025):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(0.5)
-    result = s.connect_ex((target, port))
-    
-    if result == 0:
-        print(f"Port {port} is OPEN")
-        
-    s.close()
+def scan_port(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((target, port))
+        if result == 0:
+            print(f"[+] Port {port} is OPEN")
+        sock.close()
+    except:
+        pass
 
-print("Scan completed.")
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        scan_port(port)
+        queue.task_done()
+
+for port in range(start_port, end_port + 1):
+    queue.put(port)
+
+thread_count = 100
+
+for _ in range(thread_count):
+    t = threading.Thread(target=worker)
+    t.start()
+
+queue.join()
+
+print("Scanning completed.")
